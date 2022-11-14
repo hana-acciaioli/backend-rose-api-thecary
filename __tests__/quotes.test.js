b77bf946825ad564e8a6f3daddd812f17d91f86b
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-14 15:50:44 -0800

    POST '/' route tested and working

--- __tests__/quotes.test.js
+++ __tests__/quotes.test.js
@@ -2,24 +2,26 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { Quote } = require('../lib/models/Quote');
+const { Quote } = require('../lib/models/Quote.js');
 
 describe('quotes routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.skip('should add a new quote', async () => {
-    const quote = new Quote({
-      episode_id: '6',
-      character_id: '6',
+  it('POST /quotes should add a new quote', async () => {
+    const newQuote = {
+      episodeId: '6',
+      characterId: '6',
       detail:
         'Okay, I have never heard someone say so many wrong things, one after the other, consecutively, in a row.',
-    });
-    const res = await request(app).post('/quotes').send(quote);
-    expect(res.body.episode_id).toEqual(quote.episode_id);
-    expect(res.body.character_id).toEqual(quote.character_id);
-    expect(res.body.detail).toEqual(quote.detail);
+    };
+    const res = await request(app).post('/quotes').send(newQuote);
+    expect(res.status).toBe(200);
+    console.log(res.body);
+    expect(res.body.episode_id).toEqual(newQuote.episode_id);
+    expect(res.body.character_id).toEqual(newQuote.character_id);
+    expect(res.body.detail).toEqual(newQuote.detail);
     const count = await Quote.count();
     expect(count).toEqual(10);
   });
